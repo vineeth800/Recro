c589919fa53d6613756421d521552be8f55c40e4
Shivam Rathour <66547505+shivam855@users.noreply.github.com>
2024-10-02 14:50:42 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  videoCompression: true,
+  video: true,
   screenshotOnRunFailure: false,
   env : {
     baseUrl: "https://careglp-staging.carevalidate.com/login",
@@ -11,6 +13,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return require("./cypress/plugins/index.js")(on, config);
     },
+    
     specPattern: "cypress/integration/*{.feature,.cy.js}",
     // specPattern: "cypress/integration/*.feature",
     defaultCommandTimeout: 10000
